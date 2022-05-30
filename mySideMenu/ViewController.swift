7cfbf527b826922f52a2d3776faf7120be7e70fb
Irvin <uif08816@contiwan.com>
2022-05-30 11:09:59 +0800

    Initial Commit

---
+++ mySideMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mySideMenu
+//
+//  Created by Seet, Ting Yang Irvin (uif08816) on 30/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

cad3a03e83400f6ba032d4f5111551057f169560
Irvin <uif08816@contiwan.com>
2022-05-31 09:45:46 +0800

    Add subviews and enums

---
+++ mySideMenu/InfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AboutViewController.swift
+//  mySideMenu
+//
+//  Created by Seet, Ting Yang Irvin (uif08816) on 30/5/22.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
